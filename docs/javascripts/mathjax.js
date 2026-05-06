433b7b563bf3f644708e0fdf09b8db90b9af0315
Yimin Zhong <yzz0225@auburn.edu>
2026-05-06 11:40:18 -0500

    fix \boldsymbol rendering by explicitly loading boldsymbol package in MathJax config
    Without packages: {'[+]': ['boldsymbol']}, MathJax 3 relies on
    autoload to pull in the boldsymbol extension on demand, which can
    silently fail. Adding it to the explicit package list ensures it is
    always available site-wide.

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -3,7 +3,8 @@ window.MathJax = {
     inlineMath: [["\\(", "\\)"], ["$", "$"]],
     displayMath: [["\\[", "\\]"], ["$$", "$$"]],
     processEscapes: true,
-    processEnvironments: true
+    processEnvironments: true,
+    packages: {'[+]': ['boldsymbol']}
   }
 };
 
